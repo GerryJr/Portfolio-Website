f27cbc722653f5b71ce4909e3082459ca8f2f36a
Gerardo Lopez Jr. <gerardolopezjr1178@gmail.com>
2025-12-02 14:51:12 -0800

    Auto Scroll to Top
    Now when you enter a new page, the page will automaically start at the top.

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Always scroll to top when the path changes
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null; // this component doesn't render anything
+};
